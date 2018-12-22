13e61da58b12a614b6e44f8c1048b6aa719e1ea3
leslie <441520249@qq.com>
2018-12-23 00:06:23 +0800

    周六晚

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,22 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 
+gulp.task("indexSass",function(){
+    //2.1 拿到文件流gulp.src()
+    return gulp.src("./leslie/sass/index.scss")
+    //2.2通过管道运输gulp.pipe()，运输过程中，进行sass编译 sass()
+    //*忽略错误，继续编译
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError)) 
+   
+    //2.3通过管道gulp.pipe()运输到指定目录下gulp.dest()
+    .pipe(gulp.dest("./leslie/css/"))
+})
+// 任务2：监听a.scss
+gulp.task("indexJt",function(){
+    gulp.watch("./leslie/sass/index.scss",gulp.series("indexSass"))
+})
+
+
 //2.开启gulp任务 gulp.task(任务名,函数)
 gulp.task("loginSass",function(){
     //2.1 拿到文件流gulp.src()
@@ -24,9 +40,10 @@ gulp.task("loginJt",function(){
 })
 
 
-gulp.task("indexSass",function(){
+//2.开启gulp任务 gulp.task(任务名,函数)
+gulp.task("registerSass",function(){
     //2.1 拿到文件流gulp.src()
-    return gulp.src("./leslie/sass/index.scss")
+    return gulp.src("./leslie/sass/register.scss")
     //2.2通过管道运输gulp.pipe()，运输过程中，进行sass编译 sass()
     //*忽略错误，继续编译
     .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError)) 
@@ -35,7 +52,61 @@ gulp.task("indexSass",function(){
     .pipe(gulp.dest("./leslie/css/"))
 })
 // 任务2：监听a.scss
-gulp.task("indexJt",function(){
-    gulp.watch("./leslie/sass/index.scss",gulp.series("indexSass"))
+
+gulp.task("registerJt",function(){
+    gulp.watch("./leslie/sass/register.scss",gulp.series("registerSass"))
 })
 
+
+//2.开启gulp任务 gulp.task(任务名,函数)
+gulp.task("listSass",function(){
+    //2.1 拿到文件流gulp.src()
+    return gulp.src("./leslie/sass/list.scss")
+    //2.2通过管道运输gulp.pipe()，运输过程中，进行sass编译 sass()
+    //*忽略错误，继续编译
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError)) 
+   
+    //2.3通过管道gulp.pipe()运输到指定目录下gulp.dest()
+    .pipe(gulp.dest("./leslie/css/"))
+})
+// 任务2：监听a.scss
+
+gulp.task("listJt",function(){
+    gulp.watch("./leslie/sass/list.scss",gulp.series("listSass"))
+})
+
+
+//2.开启gulp任务 gulp.task(任务名,函数)
+gulp.task("detailSass",function(){
+    //2.1 拿到文件流gulp.src()
+    return gulp.src("./leslie/sass/detail.scss")
+    //2.2通过管道运输gulp.pipe()，运输过程中，进行sass编译 sass()
+    //*忽略错误，继续编译
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError)) 
+   
+    //2.3通过管道gulp.pipe()运输到指定目录下gulp.dest()
+    .pipe(gulp.dest("./leslie/css/"))
+})
+// 任务2：监听a.scss
+
+gulp.task("detailJt",function(){
+    gulp.watch("./leslie/sass/detail.scss",gulp.series("detailSass"))
+})
+
+
+//2.开启gulp任务 gulp.task(任务名,函数)
+gulp.task("carSass",function(){
+    //2.1 拿到文件流gulp.src()
+    return gulp.src("./leslie/sass/car.scss")
+    //2.2通过管道运输gulp.pipe()，运输过程中，进行sass编译 sass()
+    //*忽略错误，继续编译
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError)) 
+   
+    //2.3通过管道gulp.pipe()运输到指定目录下gulp.dest()
+    .pipe(gulp.dest("./leslie/css/"))
+})
+// 任务2：监听a.scss
+
+gulp.task("carJt",function(){
+    gulp.watch("./leslie/sass/car.scss",gulp.series("carSass"))
+})
