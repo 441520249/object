208d328bc021ab123c5690f6d971de4a7c17a312
leslie <441520249@qq.com>
2018-12-22 08:18:45 +0800

    周五

--- gulpfile.js
+++ gulpfile.js
@@ -22,3 +22,19 @@ gulp.task("loginSass",function(){
 gulp.task("jt",function(){
     gulp.watch("./leslie/sass/login.scss",gulp.series("loginSass"))
 })
+
+
+gulp.task("indexSass",function(){
+    //2.1 拿到文件流gulp.src()
+    return gulp.src("./leslie/sass/index.scss")
+    //2.2通过管道运输gulp.pipe()，运输过程中，进行sass编译 sass()
+    //*忽略错误，继续编译
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError)) 
+   
+    //2.3通过管道gulp.pipe()运输到指定目录下gulp.dest()
+    .pipe(gulp.dest("./leslie/css/"))
+})
+// 任务2：监听a.scss
+gulp.task("jtindex",function(){
+    gulp.watch("./leslie/sass/index.scss",gulp.series("indexSass"))
+})
